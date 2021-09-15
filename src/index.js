7074090a7d1a064a51018d16529f03a83feaa234
Michael Benzinger <michaelbenzinger3@gmail.com>
2021-09-15 18:08:29 -0500

    Created basic layout for information

--- src/index.js
+++ src/index.js
@@ -2,14 +2,25 @@ import './meyerreset.css';
 import './style.css';
 let token = config.MY_API_TOKEN;
 
+function convertInput(input) {
+
+}
+
 async function getWeather(city) {
-  const h1 = document.querySelector('h1');
-  const h3 = document.querySelector('h3');
-  const data = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${token}`);
+  const temp = document.querySelector('#cwi-temp');
+  const condition = document.querySelector('#cwi-condition');
+  const feels_like = document.querySelector('#cwi-feels-like');
+  temp.innerText = 'Loading Temp';
+  condition.innerText = 'Loading Condition';
+  feels_like.innerText = 'Loading Feels Like';
+  // const data = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${token}`);
+  const data = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=Chicago,IL,US&units=imperial&appid=${token}`);
   const json = await data.json();
-  h1.innerText = 'Temp: ' + Math.round(json.main.temp) + '°F';
-  h3.innerText = json.name;
+  temp.innerText = Math.round(json.main.temp) + '°F';
+  condition.innerText = json.weather[0].main;
+  feels_like.innerText = 'Feels like ' + Math.round(json.main.feels_like) + '°F';
   console.log(json);
 }
 
-getWeather('Chicago');
+getWeather();
+// getWeather(prompt('Get weather for what city?'));
